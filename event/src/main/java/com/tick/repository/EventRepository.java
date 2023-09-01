7b1e144fa489e1f4c9558734308394fd3b0896be
Joanne Lai <joanne.lai.2021@smu.edu.sg>
2023-09-01 14:59:49 +0800

    Added category and price filters

--- event/src/main/java/com/tick/repository/EventRepository.java
+++ event/src/main/java/com/tick/repository/EventRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.tick.model.Event;
 
-public interface EventRepository extends MongoRepository<Event, String> {
+public interface EventRepository extends MongoRepository<Event, String>, CustomEventRepository {
     
 }
--- event/src/main/java/com/tick/service/EventService.java
+++ event/src/main/java/com/tick/service/EventService.java
@@ -1,18 +1,34 @@
 package com.tick.service;
 
 import org.yaml.snakeyaml.events.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.mongodb.core.query.*;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
 
 import com.tick.repository.*;
 
+@Service
 public class EventService {
     private final EventRepository eventRepository;
+    private MongoTemplate mongoTemplate;
 
-    public Event addEvent (Event e) {
-        return eventRepository.save(e);
+    public Event addEvent (Event event) {
+        return eventRepository.save(event);
     }
 
-    public List<Event> findAllEvents() {
-        return eventRepository.findAll();
+    public List<Event> filterEvents(String category, double maxPrice) {
+        Query query = new Query();
+
+        if (category != null) {
+            query.addCriteria(Criteria.where("category").is(category));
+        } 
+        if (maxPrice != null) {
+            query.addCriteria(Criteria.where("price").elemMatch(Criteria.lte(maxPrice)));
+        }
+        return mongoTemplate.find(query, Event.class);
     }
 
     public Event getEventByID (String eventID) {
@@ -30,6 +46,5 @@ public class EventService {
         eventRepository.deleteById(eventID);
         return eventID + " event deleted successfully";
     }
-
 
 }
