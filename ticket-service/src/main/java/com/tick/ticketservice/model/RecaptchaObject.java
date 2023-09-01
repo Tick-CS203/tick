961d843be375a75afd82c0afd47d1aeb0759f183
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-09-01 16:04:00 +0800

    help me

--- ticket-service/src/main/java/com/tick/ticketservice/model/RecaptchaObject.java
+++ ticket-service/src/main/java/com/tick/ticketservice/model/RecaptchaObject.java
@@ -2,7 +2,7 @@ package com.tick.ticketservice.model;
 
 public class RecaptchaObject {
     private String secret;
-    private final String response = "6Lc-W-4nAAAAAIr0NNe5umQXXf_7RWeakqN1GpvH";
+    private final String response = "";
 
     public RecaptchaObject(String secret) {
         this.secret = secret;
