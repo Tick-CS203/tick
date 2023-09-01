f94e99f5480a5ef9ecf617c7eb247fef9d62facb
Joanne Lai <joanne.lai.2021@smu.edu.sg>
2023-09-01 12:54:13 +0800

    Event service

---
+++ event/src/main/java/com/tick/model/Event.java
@@ -0,0 +1,30 @@
+package com.tick.model;
+
+import java.time.*;
+import java.util.Map;
+import lombok.Data;
+
+@Data
+public class Event {
+    private int eventID;
+    private String name;
+    private String category;
+    private String banner;
+    private LocalDateTime lastUpdated;
+    private int ticketLimit;
+    private String venueID;
+    private Map<String, Map<String, Map<String, Integer>>> seatMap;
+    private LocalDate[] dates;
+
+    public Event(String name, String category, String banner, LocalDateTime lastUpdated,
+            int ticketLimit, String venueID, Map<String, Map<String, Map<String, Integer>>> seatMap, LocalDate[] dates) {
+        this.name = name;
+        this.category = category;
+        this.banner = banner;
+        this.lastUpdated = lastUpdated;
+        this.ticketLimit = ticketLimit;
+        this.venueID = venueID;
+        this.seatMap = seatMap;
+        this.dates = dates;
+    }
+}
---
+++ event/src/main/java/com/tick/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.tick.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.tick.model.Event;
+
+public interface EventRepository extends MongoRepository<Event, String> {
+    
+}
