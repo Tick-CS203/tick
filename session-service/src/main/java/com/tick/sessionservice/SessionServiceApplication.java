09de26371bd9d3ea5842a907d8c5d2e10df9b9be
Ryan Loh <ryan.loh.2022@smu.edu.sg>
2023-09-09 00:16:23 +0800

    ACTUAL-49 refactor: moved host and port config to application.properties

--- session-service/src/main/java/com/tick/sessionservice/SessionServiceApplication.java
+++ session-service/src/main/java/com/tick/sessionservice/SessionServiceApplication.java
@@ -1,5 +1,6 @@
 package com.tick.sessionservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RestController
 @RequestMapping("/session")
 public class SessionServiceApplication {
+
+    @Value("${token-service.host}")
+    private String socketHost;
+
+    @Value("${token-service.port}")
+    private Integer socketPort;
+
     @Bean
    	public WebClient getWebClientBuilder() {
-		return WebClient.builder().baseUrl("http://localhost:8083").build();
+		return WebClient.builder().baseUrl("http://" + socketHost + ":" + socketPort).build();
    	}
 
 	public static void main(String[] args) {
