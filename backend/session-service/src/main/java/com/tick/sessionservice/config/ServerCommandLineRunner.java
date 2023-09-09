f54341f41639910877e157e500c172d79ac3bd4b
Ryan Loh <ryan.loh.2022@smu.edu.sg>
2023-09-09 17:46:45 +0800

    ACTUAL-55 refactor: moved service into backend folder

---
+++ backend/session-service/src/main/java/com/tick/sessionservice/config/ServerCommandLineRunner.java
@@ -0,0 +1,19 @@
+package com.tick.sessionservice.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.corundumstudio.socketio.SocketIOServer;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ServerCommandLineRunner implements CommandLineRunner {
+    private final SocketIOServer server;
+
+    @Override
+    public void run(String... args) throws Exception {
+        server.start();
+    }
+}
