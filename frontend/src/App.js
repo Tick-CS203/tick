137b38c581c8c8959e9f8f9f2837b86cf40d142c
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-09-11 21:59:44 +0800

    react seatmap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Recaptcha } from "./pages/Recaptcha";
+import { NationalStadium } from "./pages/Seatmap/NationalStadium";
 
 function App() {
   return (
     <div className="App">
-      <Recaptcha />
+      <NationalStadium />
     </div>
   );
 }
