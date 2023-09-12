4420f3d2bcf2524b19f742c30efc8489594764e1
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-09-12 17:28:39 +0800

    feat: routing, navbar, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from "./component/Navbar";
+import { Footer } from "./component/Footer";
+
 import { NationalStadium } from "./pages/Seatmap/NationalStadium";
+
 
 function App() {
   return (
     <div className="App">
-      <NationalStadium />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<p>HELLO</p>} />
+        <Route path="/seatmap" element={<NationalStadium />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
