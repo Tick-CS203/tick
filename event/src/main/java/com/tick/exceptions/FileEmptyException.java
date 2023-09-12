1137c40be8b4ed032714c7f8aa94aef8683f13c5
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-09-12 20:26:37 +0800

    feat: separated image service

--- event/src/main/java/com/tick/exceptions/FileEmptyException.java
+++
@@ -1,7 +0,0 @@
-package com.tick.exceptions;
-
-public class FileEmptyException extends SpringBootFileUploadException {
-   public FileEmptyException(String message) {
-       super(message);
-   }
-}
