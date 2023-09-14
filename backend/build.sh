3fc62ff4e881b081131d46f503b0a6326974412b
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-09-15 01:06:30 +0800

    Updated build script, reduced Dockerfile code

--- backend/build.sh
+++ backend/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 if [ -d backend ]; then cd backend; fi
-for file in *; do
+for file in $(ls -d */); do
     if [ -d $file ]; then
-        echo Currently in $file
+        echo "Currently in $file";
         ( cd $file;
-        if [ -f mvnw ]; then
+        if [ -f mvnw ] ; then
             ./mvnw -B package -Dmaven.test.skip;
-        else
+            cp target/*.jar app.jar;
+        elif [ -f gradlew ] ; then
             ./gradlew bootJar;
+            cp build/libs/*.jar app.jar;
+        else echo "No executable found";
         fi )
         echo
     fi
