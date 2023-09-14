adbd5dc6ffe4d69304d9ab36a5ff5833ca37af80
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-09-15 01:08:10 +0800

    Removed redundant if condition

--- backend/build.sh
+++ backend/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 if [ -d backend ]; then cd backend; fi
 for file in $(ls -d */); do
-    if [ -d $file ]; then
-        echo "Currently in $file";
-        ( cd $file;
-        if [ -f mvnw ] ; then
-            ./mvnw -B package -Dmaven.test.skip;
-            cp target/*.jar app.jar;
-        elif [ -f gradlew ] ; then
-            ./gradlew bootJar;
-            cp build/libs/*.jar app.jar;
-        else echo "No executable found";
-        fi )
-        echo
-    fi
+    echo "Currently in $file";
+    ( cd $file;
+    if [ -f mvnw ] ; then
+        ./mvnw -B package -Dmaven.test.skip;
+        cp target/*.jar app.jar;
+    elif [ -f gradlew ] ; then
+        ./gradlew bootJar;
+        cp build/libs/*.jar app.jar;
+    else echo "No executable found";
+    fi )
+    echo
 done
