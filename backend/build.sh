e9492ef76c1170c74c82d31437a31a09c6a5f194
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-09-15 16:09:42 +0800

    Changed build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 if [ -d backend ]; then cd backend; fi
-for file in *; do
-    if [ -d $file ]; then
-        echo Currently in $file
-        ( cd $file;
-        if [ -f mvnw ]; then
-            ./mvnw -B package -Dmaven.test.skip;
-        else
-            ./gradlew bootJar;
-        fi )
-        echo
-    fi
+for file in $(ls -d */); do
+    echo Currently in $file
+    ( cd $file;
+    if [ -f mvnw ]; then
+        ./mvnw -B package -Dmaven.test.skip;
+    else
+        ./gradlew bootJar;
+    fi )
+    echo
 done
