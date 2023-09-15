5e0bb936c31a6200721e0e75860c70376cc3eb93
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-09-15 17:03:05 +0800

    Modified for session service

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,10 @@ for file in $(ls -d */); do
     ( cd $file;
     if [ -f mvnw ]; then
         ./mvnw -B package -Dmaven.test.skip;
+        cp target/*.jar app.jar;
     else
         ./gradlew bootJar;
+        cp build/libs/*.jar app.jar;
     fi )
     echo
 done
