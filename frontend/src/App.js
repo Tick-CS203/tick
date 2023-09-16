1425336a9b813b5b9030333e3c372768d466bb84
Pj <ngpinjie1999@gmail.com>
2023-09-16 15:53:35 +0800

    Components skeleton
    Skeleton of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar } from "./component/Navbar";
 import { Footer } from "./component/Footer";
 import { Home } from "./pages/Home";
-
+import { Ticket } from "./pages/Ticket";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <div className="bg-black px-8">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/ticket" element={<Ticket />} />
         </Routes>
       </div>
       <Footer />
---
+++ frontend/src/pages/Ticket.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../component/ticket/TicketDetails.css';
+
+// Header Component
+const TicketHeader = ({ title }) => (
+  <h1 className="text-white font-bold text-2xl mb-4">{title}</h1>
+);
+
+// Order Details Component
+const OrderDetails = ({ orderId, orderDateTime }) => (
+  <div className="flex flex-col">
+    <p className="text-white font-semibold text-sm">Order ID: {orderId}</p>
+    <p className="text-white font-semibold text-sm">Order DateTime: {orderDateTime}</p>
+  </div>
+);
+
+// Ticket Menu Component
+const TicketMenu = () => {
+    return (
+      <div className="ticket-menu h-full flex flex-col justify-between">
+        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">My QR Code</button>
+        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Download</button>
+        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Event Details</button>
+        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Transfer Ticket to Another</button>
+      </div>
+    );
+};
+
+
+// Ticket Details Component
+const TicketDetails = ({ orderId, orderDateTime }) => {
+    return (
+      <div className="ticket-container bg-white shadow-md rounded-lg flex relative">
+        {/* Image Section */}
+        <div className="ticket-image-container">
+          <img src={"https://visitglendale.com/wp-content/uploads/2022/09/ateez.jpg"} alt="Band" className="h-full object-cover" />
+        </div>
+        {/* Details Section */}
+        <div className="ticket-details-container p-4 flex flex-col justify-between">
+          <h1 className="text-xl font-bold">My Ticket</h1>
+        </div>
+      </div>
+    );
+  };
+  
+
+// Main Ticket Component
+export const Ticket = ({ background }) => {
+    return (
+      <div className={`bg-${background} p-4`}>
+        <header className="flex flex-col justify-between">
+          <TicketHeader title="My Ticket" />
+          <OrderDetails orderId="1234567890" orderDateTime="2023-09-17 12:00:00" />
+        </header>
+        <div className="flex items-stretch"> {/* Added items-stretch for alignment */}
+          <TicketDetails />
+          <TicketMenu />
+        </div>
+      </div>
+    );
+  };
+  
+export default Ticket;
+
+
+
+
+
+
