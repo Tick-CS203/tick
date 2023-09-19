0d90971bcc4462ae4b2f3de3d45ed17409ba2daf
Joanne Lai <joanne.lai.2021@smu.edu.sg>
2023-09-19 21:03:28 +0800

    EventDetails page, axiosInstance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar } from "./component/Navbar";
 import { Footer } from "./component/Footer";
 import { Home } from "./pages/Home";
+import { EventDetails } from "./pages/EventDetails";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Navbar />
       <div className="bg-black px-8">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<EventDetails />} />
         </Routes>
       </div>
       <Footer />
