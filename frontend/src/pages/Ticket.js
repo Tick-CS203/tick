73515a373ba7e4355ed0be6710526b9aa3b69cfd
Pj <ngpinjie1999@gmail.com>
2023-09-20 16:09:56 +0800

    TicketComponent
    UI completed

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -3,12 +3,12 @@ import '../component/ticket/TicketDetails.css';
 
 // Header Component
 const TicketHeader = ({ title }) => (
-  <h1 className="text-white font-bold text-2xl mb-4">{title}</h1>
+  <h1 className="text-white font-extrabold text-3xl mb-4 font-inter italic">{title}</h1>
 );
 
 // Order Details Component
 const OrderDetails = ({ orderId, orderDateTime }) => (
-  <div className="flex flex-col">
+  <div className="flex flex-col font-inter">
     <p className="text-white font-semibold text-sm">Order ID: {orderId}</p>
     <p className="text-white font-semibold text-sm">Order DateTime: {orderDateTime}</p>
   </div>
@@ -17,11 +17,11 @@ const OrderDetails = ({ orderId, orderDateTime }) => (
 // Ticket Menu Component
 const TicketMenu = () => {
     return (
-      <div className="ticket-menu h-full flex flex-col justify-between">
+      <div className="ticket-menu h-full flex flex-col justify-between font-inter">
         <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">My QR Code</button>
         <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Download</button>
         <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Event Details</button>
-        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Transfer Ticket to Another</button>
+        <button className="menu-button text-main-yellow border-2 border-main-yellow h-full">Transfer Ticket to another</button>
       </div>
     );
 };
@@ -30,24 +30,30 @@ const TicketMenu = () => {
 // Ticket Details Component
 const TicketDetails = ({ orderId, orderDateTime }) => {
     return (
-      <div className="ticket-container bg-white shadow-md rounded-lg flex relative">
+      <div className="ticket-container bg-white shadow-md rounded-lg flex relative font-inter">
         {/* Image Section */}
         <div className="ticket-image-container">
           <img src={"https://visitglendale.com/wp-content/uploads/2022/09/ateez.jpg"} alt="Band" className="h-full object-cover" />
         </div>
         {/* Details Section */}
         <div className="ticket-details-container-top p-4 flex flex-row">
-          <h1 className="ticket-name text-xl font-bold">My Ticket</h1>
+          <h1 className="ticket-name text-xl font-bold font-lemon">ATEEZ WORLD TOUR (THE FELLOWSHIP: BREAK THE WALL) IN SINGAPORE 2023</h1>
           <div class="text-container">
               <div class="text-left">
-                  <p>Singapore Indoor Stadium</p>
-                  <p>Time</p>
+                  <p>
+                    <img src="https://cdn-icons-png.flaticon.com/128/535/535239.png" alt="Location" className="inline-block h-4 mr-2" />
+                    <span className="text-sm">Singapore Indoor Stadium</span>
+                  </p>
+                  <p>
+                    <img src="https://cdn-icons-png.flaticon.com/128/3114/3114812.png" alt="Time" className="inline-block h-4 mr-2" />
+                    <span className="text-sm">Time</span>
+                  </p>
               </div>
               <div class="text-right">
-                  <p>Day</p>
-                  <p>Date</p>
-                  <p>Month and Year</p>
-              </div>
+                <p>Friday</p>
+                <p className="text-4xl font-bold">15</p>
+                <p>November 2023</p>
+            </div>
           </div>
         </div>
         {/* <div className="details-section-right flex">
@@ -58,9 +64,11 @@ const TicketDetails = ({ orderId, orderDateTime }) => {
         <div className="details-section-right flex">
             <div className="grey-dotted-line"></div>
             <div className="test-text-container">
-                <h3 className="test-text">admit one</h3>
+                <h3 className="test-text">ADMIT ONE</h3>
             </div>
-            <div className="semi-circle"></div>
+            <div className="semi-circle-container">
+                <div className="semi-circle"></div>
+            </div>
         </div>
       </div>
     );
@@ -72,7 +80,7 @@ export const Ticket = ({ background }) => {
     return (
       <div className={`bg-${background} p-4`}>
         <header className="flex flex-col justify-between">
-          <TicketHeader title="My Ticket" />
+          <TicketHeader title="My Tickets" />
           <OrderDetails orderId="1234567890" orderDateTime="2023-09-17 12:00:00" />
         </header>
         <div className="flex items-stretch"> {/* Added items-stretch for alignment */}
