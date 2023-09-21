b992fe4ed0f8a430f820dd3585d4d50af733595e
sotongsamho <samiha12018@gmail.com>
2023-09-21 16:38:31 +0800

    edited carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar } from "./component/Navbar";
 import { Footer } from "./component/Footer";
 import { Home } from "./pages/Home";
-import { SeatSelection } from "./pages/SeatSelection";
-import { SignUp } from "./pages/SignUp";
-import { Login } from "./pages/Login";
-import { ConfirmSignUp } from "./pages/ConfirmSignUp";
+
+
 
 function App() {
   return (
@@ -16,10 +14,7 @@ function App() {
       <div className="bg-black px-8">
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/confirmsignup" element={<ConfirmSignUp />} />
-            <Route path="/seatmap" element={<SeatSelection />} />
+            
         </Routes>
       </div>
       <Footer />
