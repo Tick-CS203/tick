acd81bb9335868ca303a6a1eee4facec0c12ecd1
Sharlene Tio <75077847+ShermanNovak@users.noreply.github.com>
2023-09-23 23:06:52 +0800

    Revert "Actual 57 ticket frontend"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route } from 'react-router-dom';
-import { Navbar } from "./component/Navbar";
-import { Footer } from "./component/Footer";
-import { Home } from "./pages/Home";
-import { Ticket } from "./pages/Ticket";
+import { Recaptcha } from "./pages/Recaptcha";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="bg-black px-8">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ticket" element={<Ticket />} />
-        </Routes>
-      </div>
-      <Footer />
+      <Recaptcha />
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,17 +2,8 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        "main-yellow": "#F6E902",
-        "main-blue": "#7DCCEC",
-      },
-      fontFamily: {
-        inter: ["Inter", "Helvetica"],
-        lemon: ["Lemon"],
-        mono: ["Azeret Mono"]
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
