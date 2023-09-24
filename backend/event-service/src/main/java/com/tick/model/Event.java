27c5f01bd90ec33200017a81db394cd44f964d91
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-09-24 16:03:06 +0800

    Added CorsFilter for all endpoints, added description

--- backend/event-service/src/main/java/com/tick/model/Event.java
+++ backend/event-service/src/main/java/com/tick/model/Event.java
@@ -24,7 +24,7 @@ class Links {
 public class Event {
 
     @Id
-    private int eventId;
+    private int eventID;
     private String name;
     private String description;
     private String category;
@@ -38,9 +38,10 @@ public class Event {
     private List<EventDate> date;
     private Links links;
 
-    public Event(String name, String category, String banner, LocalDateTime lastUpdated, List<Price> prices,
+    public Event(String name, String description, String category, String banner, LocalDateTime lastUpdated, List<Price> prices,
             int ticketLimit, String venueId, Map<String, Map<String, Map<String, Integer>>> seatMap, List<EventDate> date, Links links) {
         this.name = name;
+        this.description = description;
         this.category = category;
         this.banner = banner;
         this.lastUpdated = lastUpdated;
