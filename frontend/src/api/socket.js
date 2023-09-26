6823a1b5f385a227a462de6804c9565f136752fb
Ryan Loh <ryan.loh.2022@smu.edu.sg>
2023-09-26 14:53:16 +0800

    fix: missing room query param causing server error

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -1,3 +1,10 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-export const socket = io("ws://service-alb-832968326.ap-southeast-1.elb.amazonaws.com/");
+export const socket = io(
+  "ws://service-alb-832968326.ap-southeast-1.elb.amazonaws.com/",
+  {
+    query: {
+      room: "eventID1",
+    },
+  }
+);
