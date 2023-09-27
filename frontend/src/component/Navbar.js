775de8eceea9b4ea35d967323a387706555acc89
Joanne Lai <joanne.lai.2021@smu.edu.sg>
2023-09-28 01:23:35 +0800

    Fixed nav bar component

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <nav className="bg-black">
+    <nav className="bg-black w-full sticky top-0 z-50">
       <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-8">
         <button className="font-lemon text-main-yellow text-xl">
         <Link to="/">TICK</Link>
