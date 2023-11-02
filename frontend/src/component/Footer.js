cdd333e70a6a0660731d3fdd032c147c41245c25
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-11-02 18:58:26 +0800

    Revert "Revert "Actual 57 ticket frontend""
    This reverts commit acd81bb9335868ca303a6a1eee4facec0c12ecd1.

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <div className="flex flex-col gap-y-1 bg-main-yellow text-left py-4 px-8">
+      <p className="font-lemon text-black text-xl">TICK</p>
+      <p className="font-inter text-black text-xs">
+        © 2023 Tick. All rights reserved.
+      </p>
+    </div>
+  );
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ Amplify.configure(awsconfig);
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
