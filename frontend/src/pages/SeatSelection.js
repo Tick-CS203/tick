0a0482b57caac3dedaded8b1afeb68d901887b96
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-11-02 11:31:43 +0800

    clean import

--- frontend/src/pages/SeatSelection.js
+++ frontend/src/pages/SeatSelection.js
@@ -2,7 +2,7 @@ import { Steps, Select } from "antd";
 import { useState, useEffect } from "react";
 import { NationalStadium } from "../component/seatselection/NationalStadium";
 import { RowData } from "../component/seatselection/RowData";
-import { axiosInstance, axiosLocalHostInstance2 } from "../api/axios";
+import { axiosInstance } from "../api/axios";
 import { useParams } from "react-router-dom";
 import { useEventQuery } from "../api/events.query";
 import { useSelector } from "react-redux";
