e9c267821a0e38e970b09b9321a37fffe280c36f
sotongsamho <samiha12018@gmail.com>
2023-11-04 18:47:15 +0800

    retrive venue from event

---
+++ frontend/src/api/venue.query.js
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getVenue } from "../service/venue.service";
+
+export const useVenueQuery = (venue_id) => {
+    return useQuery({
+        queryKey: ["venue", venue_id],
+        queryFn: () => getVenue(venue_id),
+    });
+};
