e9c2f8828818b77902fc3c43b29d347c32986dbe
Zachary-Tan-2022 <zachary.tan.2022@scis.smu.edu.sg>
2023-11-06 00:50:25 +0800

    Added navigate on bookmarks

--- frontend/src/api/bookmarks.query.js
+++ frontend/src/api/bookmarks.query.js
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookmarks } from "../service/bookmarks.service";
+import { useNavigate } from "react-router-dom";
 
 export const useBookmarks = (accessToken) => {
+    const navigate = useNavigate()
     return useQuery({
         queryKey: ["bookmarks"],
-        queryFn: () => getBookmarks(accessToken),
+        queryFn: () => getBookmarks(accessToken, navigate),
     });
 };
