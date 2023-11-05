781908931e80ec5f18b5f0d1328631ffe8330673
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-11-05 13:53:11 +0800

    feat: frontend recommended events

--- frontend/src/pages/EventDetails.js
+++ frontend/src/pages/EventDetails.js
@@ -1,13 +1,16 @@
-import { useEventQuery } from "../api/events.query.js";
+import {
+  useEventQuery,
+  useRecommendedEventsQuery,
+} from "../api/events.query.js";
 import { useParams } from "react-router-dom";
-import React from "react";
 import { Link } from "react-router-dom";
+import { Event } from "../component/homepage/Event.js";
 
 export const EventDetails = () => {
   const { id } = useParams();
 
   const { data: event, isLoading, isSuccess, isError } = useEventQuery(id);
-  console.log(event);
+  const { data: recommendedEvents } = useRecommendedEventsQuery(event?.artist);
 
   const formatEventDateTime = (dateTimeString) => {
     const options = {
@@ -66,10 +69,10 @@ export const EventDetails = () => {
                   <span className="text-white pl-5 font-semibold">
                     {event.date && event.date.length > 0
                       ? event.date.map((eventDate, index) => (
-                        <span key={index}>
-                          {formatEventDateTime(eventDate.eventDateTime)}
-                        </span>
-                      ))
+                          <span key={index}>
+                            {formatEventDateTime(eventDate.eventDateTime)}
+                          </span>
+                        ))
                       : "Date not available"}
                   </span>
                 </div>
@@ -154,6 +157,27 @@ export const EventDetails = () => {
                   e-tickets.
                 </p>
               </div>
+              {recommendedEvents && recommendedEvents.length > 0 && (
+                <div className="py-5">
+                  <p className="font-inter font-black text-white italic text-2xl">
+                    RECOMMENDATIONS BASED ON SIMILAR ARTISTS
+                  </p>
+
+                  <div className="flex flex-row gap-4 pt-5 overflow-hidden">
+                    {recommendedEvents.map((event) => {
+                      return (
+                        <Event
+                          key={event.eventID}
+                          eventId={event.eventID}
+                          imageURL={event.banner}
+                          eventName={event.name}
+                          eventDates={event.date}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
             </div>
 
             <div className="pt-5 lg:w-2/12 w-full min-w-fit flex flex-col items-start space-y-3">
