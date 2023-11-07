476cd13ed62768ee11e31d9700cf59ec3a5e0b55
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-11-07 22:52:40 +0800

    filter home page events

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -55,15 +55,15 @@ export const Navbar = () => {
               onClick={() => setShowSidebar((prevState) => !prevState)}
             >
               <svg
-                class="w-6 h-6"
+                className="w-6 h-6"
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  clip-rule="evenodd"
-                  fill-rule="evenodd"
+                  clipRule="evenodd"
+                  fillRule="evenodd"
                   d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
                 ></path>
               </svg>
@@ -132,7 +132,7 @@ export const Navbar = () => {
                   >
                     <g
                       fill="#f6e902"
-                      fill-rule="nonzero"
+                      fillRule="nonzero"
                       stroke="none"
                       stroke-width="1"
                       stroke-linecap="butt"
